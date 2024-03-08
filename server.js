00038276ff3d308c09b7ea01877da7da2fe1e810
Your GitHub Username <your.email@example.com>
2024-03-08 17:36:12 +0530

    modified server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {connectToDatabase}=require('./db');
 const connectorRoutes = require('./routes/connectorRoutes');
 
 function startServer(port) {
@@ -21,11 +20,6 @@ function stopServer(server) {
     console.log('Server stopped');
   });
 }
-const PORT=3000;
-const MONGO_URL='mongodb://127.0.0.1:27017/ChargeStationDB';
-
-startServer(PORT);
-connectToDatabase(MONGO_URL);
 
 module.exports = {
   startServer,
