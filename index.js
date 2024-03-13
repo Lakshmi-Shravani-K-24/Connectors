1a4a44f646890fe4a0d8efbd75343faf94b07e09
Your GitHub Username <your.email@example.com>
2024-03-13 13:08:38 +0530

    modified index

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
-const PORT=3000;
+
 process.env.MONGO_URL='mongodb://127.0.0.1:27017/ChargeStationDB';
 const {connectToDatabase, startServer}=require('./serverAndDbStart');
 async function establishDatabaseConnection() {
   await connectToDatabase(process.env.MONGO_URL);
 }
-
-
+establishDatabaseConnection();
+const PORT=3000;
 const app=startServer(PORT);
 
-establishDatabaseConnection();
+
 module.exports={app};
--- test/testProductionConnection.js
+++ test/testProductionConnection.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const sinon = require('sinon');
 const assert = require('assert');
 const {consoleLogStub}=require('./testCrud');
@@ -14,16 +14,10 @@ describe('Testing Production server and Database', ()=>{
     const {closeDatabaseConnection}=require('../serverAndDbClose');
     await closeDatabaseConnection();
   });
-  describe('Production Server and Database Start Tests', function() {
+  describe('Production Server Start', function() {
     it('should check if the production server is started and message is logged', function() {
       assert(server, 'Production Server is not started');
       sinon.assert.calledWith(consoleLogStub, 'Server is listening on port 3000');
-    });
-    it('should check if production database is connected and message is logged', async function() {
-      const isProductionDatabaseConnected = mongoose.connection.readyState === 1;
-      assert(isProductionDatabaseConnected, 'Production Database is not connected');
-      sinon.assert.calledWith(consoleLogStub, 'Connected to Database');
-      consoleLogStub.restore();
     });
   });
 });
